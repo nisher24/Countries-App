3601ef7a5cdb25aa215cbe3b890909f6042df8b1
Yuxing <yxdeng@hotmail.com>
2023-01-29 17:07:21 -0500

    Add country basic info modal

--- public/js/main.js
+++ public/js/main.js
@@ -3,17 +3,78 @@
 const searchCountryInput = document.querySelector('#search-country-input');
 const searchCountryButton = document.querySelector('#search-country-button');
 
-// Add a click event listener to the submit button
-searchCountryButton.addEventListener('click', function() {
+function displayCountryInfo() {
   // Get the value of the input element
   const searchCountryValue = searchCountryInput.value;
 
   // Send a GET request
   axios.get(`https://restcountries.com/v3.1/name/${searchCountryValue}?fullText=true`)
     .then(function (response) {
-      console.log(response.data);
+      // Get the response JSON data
+      let countryData = response.data;
+      console.log(countryData);
+
+      // Update the modal content with JSON data
+      let countryInfoList = document.querySelector('#country-info-list');
+
+      // Add country name
+      let countryName = document.querySelector('#country-basic-info-modal-title');
+      countryName.innerHTML = countryData[0].name.official;
+
+      // Add capital city
+      let capitalCity = document.createElement('li');
+      capitalCity.classList.add('list-group-item');
+      capitalCity.innerHTML = "<b>Capital city: </b> " + countryData[0].capital[0];
+      countryInfoList.appendChild(capitalCity);
+
+      // Add region
+      let region = document.createElement('li');
+      region.classList.add('list-group-item');
+      region.innerHTML = "<b>Region: </b> " + countryData[0].region;
+      countryInfoList.appendChild(region);
+
+      // Add currency
+      let currency = document.createElement('li');
+      currency.classList.add('list-group-item');
+      let currencyCode = Object.keys(countryData[0].currencies)[0];
+      currency.innerHTML = "<b>Currency: </b> " + countryData[0].currencies[currencyCode].name + " (" + currencyCode + ")";
+      countryInfoList.appendChild(currency);
+
+      // Add languages
+      let language = document.createElement('li');
+      language.classList.add('list-group-item');
+      let lanCodes = Object.keys(countryData[0].languages);
+      let lans = "";
+
+      for (let i = 0; i < lanCodes.length - 1; i++) {
+        let lanCode = lanCodes[i];
+        let lan = countryData[0].languages[lanCode];
+        lans += lan;
+        lans += ", ";
+      }
+      lans += countryData[0].languages[lanCodes[lanCodes.length - 1]];
+
+      language.innerHTML = "<b>Languages: </b> " + lans;
+      countryInfoList.appendChild(language);
+
+      // Add population
+      let population = document.createElement('li');
+      population.classList.add('list-group-item');
+      population.innerHTML = "<b>Population: </b> " + countryData[0].population;
+      countryInfoList.appendChild(population);
+
+      // Show modal
+      $('#country-basic-info-modal').modal('show');
     })
     .catch(function (error) {
       console.error(error);
     });
+}
+
+// Add a click event listener to the search country button
+searchCountryButton.addEventListener('click', displayCountryInfo);
+
+// Clear country basic info modal content when close it
+$("#country-basic-info-modal").on("hidden.bs.modal", function () {
+  $("#country-info-list").empty();
 });
